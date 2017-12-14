c3aad5af00deb594082ab4c4c9685aff116a6edc
Sergio Camargo <sergio.camargo813@gmail.com>
2017-12-13 21:47:08 -0800

    create canvas

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 12:15:26 by scamargo          #+#    #+#             */
-/*   Updated: 2017/12/12 22:16:55 by scamargo         ###   ########.fr       */
+/*   Updated: 2017/12/13 17:50:20 by scamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 int		main(int argc, char **argv)
 {
-	char *input;
+	char	*input;
+	char	*output;
 	if (argc != 2)
 	{
 		ft_putstr("usage: ./fillit input_file\n");
@@ -26,7 +27,11 @@ int		main(int argc, char **argv)
 		ft_putstr("error\n");
 		return (2);
 	}
-	ft_putstr("input is valid\n");
-	ft_putstr(find_square(input));
+	if(!(output = find_square(input)))
+	{
+		ft_putstr("malloc error\n");
+		return (3);
+	}
+	ft_putstr(output);
 	return (0);
 }
--- find_square.c
+++ find_square.c
@@ -6,12 +6,43 @@
 /*   By: scamargo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 22:09:23 by scamargo          #+#    #+#             */
-/*   Updated: 2017/12/12 22:41:27 by scamargo         ###   ########.fr       */
+/*   Updated: 2017/12/13 21:39:43 by scamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static char	*init_canvas(int num_of_tetriminos)
+{
+	int		side_length;
+	char	*canvas;
+	int		i;
+
+	side_length = (num_of_tetriminos * 4);
+	if(!(canvas = ft_memalloc((side_length + 1) * side_length + 1)))
+	   return (NULL);
+	i = 0;
+	while (i < ((side_length + 1) * side_length))
+	{
+		if (i % (side_length + 1) == side_length)
+			canvas[i] = '\n';
+		else
+			canvas[i] = '.';
+		i++;
+	}
+	canvas[i] = '\0';
+	return (canvas);
+}					
+
 char	*find_square(char *input)
 {
-	input[20] = '\0'; //TODO: just for testing
-	return (input);
+	int		number_of_tetriminos;
+	char	*canvas;
+
+	number_of_tetriminos = 4; //TODO: remove!!
+	if(!(canvas = init_canvas(number_of_tetriminos)))
+		return (NULL);
+	//trim_canvas(&canvas);
+	ft_strlen(input); //TODO: remove!!
+	return (canvas);
 }
